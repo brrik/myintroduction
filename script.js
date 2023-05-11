99c332ff88af39b50632c4689cf710faf16090ad
brrik <60531730+brrik@users.noreply.github.com>
2023-05-11 23:52:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let positionTop     = 0;
 
 window.addEventListener("scroll",(e) =>{
-	positionTop  = window.scrollY;
+    positionTop  = window.scrollY;
     console.log(positionTop);
     if(positionTop>1100){
         document.getElementById("MainDiv").style.display = "none";
