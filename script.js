07fe205b9e765bca4a60dcc2d0c0e1e4de3abc94
brrik <60531730+brrik@users.noreply.github.com>
2023-05-11 23:20:55 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let positionTop     = 0;
+
+window.addEventListener("scroll",(e) =>{
+	positionTop  = window.scrollY;
+    console.log(positionTop);
+    if(positionTop>1000){
+        document.getElementById("MainDiv").style.display = "none";
+        document.getElementById("ErrDiv").style.display = "block";
+    }
+});
