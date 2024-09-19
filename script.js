17897c47300b9aeff34ee44ecf803b60e2c833fa
brrik <60531730+brrik@users.noreply.github.com>
2024-09-19 10:36:38 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const scrollHeight = document.getElementById("MainDiv").offsetHeight+30;
+const scrollHeight = document.getElementById("MainDiv").offsetHeight+60;
 const pageMostBottom = scrollHeight - window.innerHeight - 20;
 let PhotoBln = false;
 
